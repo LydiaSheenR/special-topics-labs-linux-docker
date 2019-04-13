03cf03e11032054cc9500d083419221db48fb8c6
LydiasheenR <lydiatomy@gmail.com>
2019-04-13 16:43:43 -0400

    Added more to shell script

--- bin/gitlab-ctl.sh
+++ bin/gitlab-ctl.sh
@@ -1,3 +1,77 @@
 #! /bin/bash
 
-docker-compose up
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Please supply atleast one argument'
+    exit 1
+fi
+
+echo $1 are not $2
+
+if [[ $# -eq 1 ]] ; then
+    option="${1}"
+        case ${option} in
+           -h) FILE="${2}"
+              echo "help for shell script"
+              exit 0
+              ;;
+           -d) DIR="/tmp/gitlab-data"
+              echo "Dir name is $DIR"
+              ;;
+           -p) PORT= '30080'
+              echo "Port GitLab listens on $PORT"
+              ;;
+           -start)
+              echo "Starting gitlab in docker"
+              docker-compose up -d
+              ;;
+           -stop)
+              echo "Stopping Gitlab in docker"
+              docker-compose up -d
+              ;;
+           -destroy)
+              echo "Stopping Gitlab and removing docker containers"
+              docker-compose up -d
+              ;;
+           *)
+              echo "test"
+              exit 1 # Command to come out of the program with status 1
+              ;;
+        esac
+
+else
+    option="${2}"
+        case ${option} in
+           -h)
+              echo "help for shell script"
+              exit 0
+              ;;
+           -d) DIR="/tmp/gitlab-data"
+              echo "Dir name is $DIR"
+              ;;
+           -p) PORT= "30080"
+              echo "Port GitLab listens on $PORT"
+              ;;
+           start)
+              echo "Starting gitlab in docker"
+              docker-compose up -d
+              ;;
+           stop)
+              echo "Stopping Gitlab in docker"
+              docker-compose stop -d
+              ;;
+           destroy)
+              echo "Stopping Gitlab and removing docker containers"
+              docker-compose down -d
+              ;;
+           *)
+              echo "test"
+              exit 1 # Command to come out of the program with status 1
+              ;;
+        esac
+   exit 1
+fi
+
+
+
+#docker-compose up -d
