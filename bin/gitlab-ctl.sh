0064274df45aae6f790355d2e58dd61f8b951064
LydiasheenR <lydiatomy@gmail.com>
2019-04-13 17:13:50 -0400

    made more changes to shell script

--- bin/gitlab-ctl.sh
+++ bin/gitlab-ctl.sh
@@ -4,43 +4,9 @@
 if [[ $# -eq 0 ]] ; then
     echo 'Please supply atleast one argument'
     exit 1
-fi
-
-echo $1 are not $2
-
-if [[ $# -eq 1 ]] ; then
-    option="${1}"
-        case ${option} in
-           -h) FILE="${2}"
-              echo "help for shell script"
-              exit 0
-              ;;
-           -d) DIR="/tmp/gitlab-data"
-              echo "Dir name is $DIR"
-              ;;
-           -p) PORT= '30080'
-              echo "Port GitLab listens on $PORT"
-              ;;
-           -start)
-              echo "Starting gitlab in docker"
-              docker-compose up -d
-              ;;
-           -stop)
-              echo "Stopping Gitlab in docker"
-              docker-compose up -d
-              ;;
-           -destroy)
-              echo "Stopping Gitlab and removing docker containers"
-              docker-compose up -d
-              ;;
-           *)
-              echo "test"
-              exit 1 # Command to come out of the program with status 1
-              ;;
-        esac
 
 else
-    option="${2}"
+    option="${1}"
         case ${option} in
            -h)
               echo "help for shell script"
@@ -49,8 +15,8 @@ else
            -d) DIR="/tmp/gitlab-data"
               echo "Dir name is $DIR"
               ;;
-           -p) PORT= "30080"
-              echo "Port GitLab listens on $PORT"
+           -p)
+              echo " GitLab listens on PORT 30080"
               ;;
            start)
               echo "Starting gitlab in docker"
